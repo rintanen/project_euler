5d3309616cf699e6b1f0136ac682e50c9ad93a45
rintanen <pee.rintanen@gmail.com>
2023-03-09 12:35:35 +0200

    comment

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,4 +1,8 @@
 fn main() {
+    /*
+    Find the difference between the sum of the squares
+    of the first one hundred natural numbers and the square of the sum
+     */
     let n = 100;
     let sum: u32 = (n * n + n) / 2;
     let sum_of_squares: u32 = n * (n + 1) * (2 * n + 1) / 6;
