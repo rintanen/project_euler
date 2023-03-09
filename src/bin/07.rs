3208c521cc35841a30ead7bb298659b259545336
rintanen <pee.rintanen@gmail.com>
2023-03-09 13:52:32 +0200

    fda

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,7 +4,6 @@ use primes::{Sieve, PrimeSet};
 fn main() {
     // What is the 10 001st prime number?
     let n = 10001;
-    let mut psieve = Sieve::new();
-    let nth_prime: u64 = psieve.iter().skip(n-1).take(1).next().unwrap();
+    let nth_prime: u64 = Sieve::new().iter().nth(n).unwrap();
     println!("{n}th prime is: {nth_prime}");
 }
